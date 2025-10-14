0d072d4c6799e9ee539eb07c33093c6478c38756
Eva Grimes <evagelinagrimes@gmail.com>
2025-10-14 00:35:52 -0400

    Added temporary constants to data

---
+++ data.js
@@ -0,0 +1,33 @@
+const themes = [
+    "Superheroes", "Villians", "Disney Characters", 
+    "Decades", "Hollywood Glamour", "Pirates", "Cowboys & Cowgirls",
+    "Fairy Tales", "Alice in Wonderland", "Masquerade",
+    "Gothic/Victorian", "Angels & Devils", "Witches & Wizards",
+    "Movie Characters",  "Monsters", "Zombies", "Vampires", 
+    "TV Show Characters", "Video Game Characters", "Historical Figures",
+    "Celebrities", "Punk Rock", "Disco", "Animals", "Space/Sci-Fi",
+    "Steampunk", "Greek Gods & Goddesses", "Candy Land", "Emojis",
+    "Gender Swap", "Horror Movie"
+]
+
+const locations = [
+    { name: "Living room", cost: 0 },
+    { name: "Restaurant", cost: 2 },
+    { name: "Bar", cost: 3 }, 
+    { name: "Living Room", cost: 0},
+    { name: "Airbnb", cost: 3},
+    { name: "Kitchen", cost: 0},
+    { name: "Restaurant", cost: 2},
+    { name: "Bar", cost: 3}, 
+    { name: "Coffee Shop", cost: 1},
+    { name: "Park", cost: 0}
+]
+
+const activities = [
+    { name: "Journaling/scrap booking together", cost: 1 },
+    { name: "Stargazing", cost: 0 },
+    { name: "Face masks & manicures", cost: 2 },
+    { name: "Painting (canvas or pottery)", cost: 1 },
+    { name: "Cookie decorating", cost: 2 },
+    { name: "Board games", cost: 0 },
+]
